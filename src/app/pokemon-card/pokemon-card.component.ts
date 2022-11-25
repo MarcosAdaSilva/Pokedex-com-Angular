277658792abd085265c34b0a191d1eeb89859fe6
MarcosAdaSilva <marcaofly@gmail.com>
2022-11-25 00:52:26 -0300

    Utilizando componentes para criação de pokemons

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.css']
+  styleUrls: ['./pokemon-card.component.css'],
 })
 export class PokemonCardComponent {
-
+  @Input('pokemon')
+  public pokemon: PokemonListComponent;
 }
